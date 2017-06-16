fe8d109982c595c6968ac1581cbc41bb009d5837
Fitssum Haile <fitsum.wolde@gmail.com>
2017-06-16 15:26:10 -0400

    ng-repeat rendered

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-let superheroData = [
-  { name:'Batman', phone_number:'202-456-2234', comics_affiliation:'humanity', image_url:'http://i.imgur.com/WPhp4m3.jpg'  },
-  { name:'Hulk', phone_number:'202-786-0098', comics_affiliation:'humanity', image_url:'http://i.imgur.com/QOpbFG4.jpg' },
-  { name:'Spiderman', phone_number:'334-234-8976', comics_affiliation:'humanity', image_url:'http://i.imgur.com/t7ZFgdE.jpg' },
-  { name:'Superwoman', phone_number:'345-789-9908', comics_affiliation:'humanity', image_url:'http://i.imgur.com/Jr6nXBL.jpg' },
-  { name:'Superman', phone_number:'202-909-0098', comics_affiliation:'humanity', image_url:'http://i.imgur.com/EYSkgcY.jpg' },
-  { name:'Wonderwoman', phone_number:'202-786-2231', comics_affiliation:'humanity', image_url:'http://i.imgur.com/CtAkTsJ.jpg' }
+let superheroesData = [
+  { name:'Batman', phone_number:'202-456-2234', comics_affiliation:'humanity', image_url:'http://i.imgur.com/WPhp4m3.jpg', completed:true },
+  { name:'Hulk', phone_number:'202-786-0098', comics_affiliation:'humanity', image_url:'http://i.imgur.com/QOpbFG4.jpg', completed:true },
+  { name:'Spiderman', phone_number:'334-234-8976', comics_affiliation:'humanity', image_url:'http://i.imgur.com/t7ZFgdE.jpg', completed:true },
+  { name:'Superwoman', phone_number:'345-789-9908', comics_affiliation:'humanity', image_url:'http://i.imgur.com/Jr6nXBL.jpg', completed:true },
+  { name:'Superman', phone_number:'202-909-0098', comics_affiliation:'humanity', image_url:'http://i.imgur.com/EYSkgcY.jpg', completed:true },
+  { name:'Wonderwoman', phone_number:'202-786-2231', comics_affiliation:'humanity', image_url:'http://i.imgur.com/CtAkTsJ.jpg', completed:true }
 ]
 
 angular
-  .module("superheroApp", [])
-  .controller("SuperheroController", [SuperheroControllerFunction])
+  .module("superheroesApp", [])
+  .controller("SuperheroesController", [SuperheroesControllerFunction])
+
+  function SuperheroesControllerFunction () {
+    this.superheroes = superheroesData
+    this.addSuperheroes = function() {
+      let superheroes = {title: this.newSuperheroes, completed: true}
+      this.superheroes.push(superheroes)
+  };
+};
