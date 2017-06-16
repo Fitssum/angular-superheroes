76c41b07474a31fd73c1ad581614e55b5d4327ed
Fitssum Haile <fitsum.wolde@gmail.com>
2017-06-16 16:49:57 -0400

    final submission

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 let superheroesData = [
-  { name:'Batman', phone_number:'202-456-2234', comics_affiliation:'humanity', image_url:'http://i.imgur.com/WPhp4m3.jpg', completed:true },
-  { name:'Hulk', phone_number:'202-786-0098', comics_affiliation:'humanity', image_url:'http://i.imgur.com/QOpbFG4.jpg', completed:true },
-  { name:'Spiderman', phone_number:'334-234-8976', comics_affiliation:'humanity', image_url:'http://i.imgur.com/t7ZFgdE.jpg', completed:true },
-  { name:'Superwoman', phone_number:'345-789-9908', comics_affiliation:'humanity', image_url:'http://i.imgur.com/Jr6nXBL.jpg', completed:false },
-  { name:'Superman', phone_number:'202-909-0098', comics_affiliation:'humanity', image_url:'http://i.imgur.com/EYSkgcY.jpg', completed:false },
-  { name:'Wonderwoman', phone_number:'202-786-2231', comics_affiliation:'humanity', image_url:'http://i.imgur.com/CtAkTsJ.jpg', completed:false }
+  { name:'batman', phone_number:'202-456-2234', comics_affiliation:'power', image_url:'http://i.imgur.com/WPhp4m3.jpg', completed:true },
+  { name:'hulk', phone_number:'202-786-0098', comics_affiliation:'anger', image_url:'http://i.imgur.com/QOpbFG4.jpg', completed:true },
+  { name:'spiderman', phone_number:'334-234-8976', comics_affiliation:'fear', image_url:'http://i.imgur.com/t7ZFgdE.jpg', completed:true },
+  { name:'superwoman', phone_number:'345-789-9908', comics_affiliation:'humanity', image_url:'http://i.imgur.com/Jr6nXBL.jpg', completed:false },
+  { name:'superman', phone_number:'202-909-0098', comics_affiliation:'fictious', image_url:'http://i.imgur.com/EYSkgcY.jpg', completed:false },
+  { name:'wonderwoman', phone_number:'202-786-2231', comics_affiliation:'graceful', image_url:'http://i.imgur.com/CtAkTsJ.jpg', completed:false }
 ]
 
 angular
